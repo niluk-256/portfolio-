b806fd4238520fa96a42e52dfcbbaa1e70a04fb8
Neluka <nelukarulzz@gmail.com>
2022-11-08 20:37:04 +0530

    updated

---
+++ pages/main.js
@@ -0,0 +1,27 @@
+import { Alchemy, Network } from "alchemy-sdk"
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API,
+  network: Network.MATIC_MAINNET,
+}
+const alchemy = new Alchemy(config)
+// Fetch all the NFTs owned by elanhalpern.eth
+const main = async () => {
+  // Get all NFTs
+  const nfts = await alchemy.nft.getNftsForOwner(
+    "0x69E65a4D1b9497575A403b0cbb32f59d53268E26"
+  )
+  // Print NFTs
+  console.log(nfts)
+}
+// Execute the code
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+runMain()
